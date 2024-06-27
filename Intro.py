57a0cf946aef645d41a786eee33cd99c5777a57d
Krish <krishdhanani9989@gmail.com>
2024-06-27 12:21:18 +0530

    27 Jun

---
+++ Intro.py
@@ -0,0 +1,27 @@
+# What include this file:
+# Intro about how data is transferring among the internet
+
+# Web application technologies:
+# Browser: is Build upon HTML, CSS, DOM, JavaScript, JQuery etc.
+# Web Server: is Build upon Django, Flask, Sqlite3, Mysql etc
+
+# Each time a user clicks on anchor tag with a href = value to switch to a new page, the browser make connection to a web server and issue a "GET" request - to GET the content of the page at the specific URL.
+# The server returns the HTML document to the browser, which formats and displays the document to the user.
+
+
+# (Some Computer Network Portion need to understand)
+# Network Sockets:
+# Socket: combination of ip address and port number
+# HTTP (Hyper text transfer protocol) protocol uses to communicate with web server.
+# URL: Uniform Resource Locator (e.g. http://data.pre4e.org:5050/page1.htm)
+# http://  <-- protocol
+# data.pre4e.org  <-- host name
+# 5050 <-- port number
+# page1.htm  <-- document(page)
+
+# Flow of web request:
+# 'Model-View-Controller' (MVC) bases the request response was happening
+# in Model: Business logic
+# in View: Presentation Logic
+# Controller handel both Model and View
+
