fb482bc328c31f3c9ed9838187909d84e025832e
Krish <krishdhanani9989@gmail.com>
2024-07-01 20:11:12 +0530

    1 July

--- Section_2/Introduction.py
+++ Section_2/Introduction.py
@@ -1,8 +1,25 @@
 # What this Module cover:
-# End-of-the-Day: Monthly Challenges
+# End-of-the-Day: Monthly Challenges Website
 # 1.What are "URLs" (or "Routes") & "Views"?
 # 2.Understanding Requests & Response
 # 3.Delivering Content & Processing Input
 
+# here we create section 2 and inside that "monthly_challenges" name one project is
+#  there, and inside it there is challenge name app is there.
+
+# What are Url's Config & Routes? :
+# URL-Action mapping that ensures that certain results are "archived" when certain URL enter by user.
+
+# e.g.
+# my-page.com/ --> show starting page
+# my-page.com/posts --> show list of all posts
+# my-page.com/posts/python-is-great --> show specific post
+
+# It's like give different URL's and get back different results.
+
+# What are Views?:
+# The logic that is executed for different URLs(and Http method)
+# And there are many "functions" and "class" in code that handle request and response.
+# What they do?: Load and prepare data; Run any other Business logic; prepare and return response data(e.g. HTML)
 
 
