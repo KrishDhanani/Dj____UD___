289ce03d40889e79c546101ffaa021265e030f79
Krish <krishdhanani9989@gmail.com>
2024-08-01 22:54:35 +0530

    1 Aug

---
+++ Section_9/Introduction.py
@@ -0,0 +1,10 @@
+# What include this file:
+# User Input, Forms, & Class Based Views
+# Using user input & Improving view logic
+
+# Module Content
+# Creating & Handling Forms
+# Simplifying Form Management
+# Exploring Class Based Views
+
+# First we make a form and also add some basic setting to start Django app.
