44192eabfa521569879c616fd84db44274008eeb
Krish <krishdhanani9989@gmail.com>
2024-08-24 21:45:56 +0530

    24 Aug

--- Section_9/Feedback/reviews/views.py
+++ Section_9/Feedback/reviews/views.py
@@ -1,8 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def review(request):
-    return render(request, 'reviews/review.html')
 
-# hello
+    if request.method == 'POST':        # Here we check wether submitted form is submit by POST metod or not.
+        entered_username = request.POST['username']     
+        # Here request means read some data from request
+        # Then POST is method through which data was submitted and it hold dictionary of data. 
+        # Here name is act as dictionary's key and by which we gwt data 
+        print(entered_username)
+        # return render(request, 'reviews/thankyou.html')
+        # we can't(could but not recomanded) render template bcs POST method use to post data to the server so we use another function here.
+        if entered_username == '' or len(entered_username)>100: # Manually validation check
+            return render(request, 'reviews/review.html', {
+                'has_error': True
+            })
+        return HttpResponseRedirect('/thank-you')
+    
+    return render(request, 'reviews/review.html', {
+        'has_error': False
+    })
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
--- Section_9/Introduction.py
+++ Section_9/Introduction.py
@@ -7,14 +7,18 @@
 # Simplifying Form Management
 # Exploring Class Based Views
 
-# First we make a form and also add some basic setting to start Django app.
+# So till now whichever data we are inserted through admin pannel is for only owner of that website for insert from user data we need forms.
 
+#  So first we make a form and also add some basic setting to start Django app.
+
+# Run Code.
 # Now see there if after filing some value inside the input and you hit send button then see browser was reload and also at the url after
 # '?' mark whichever value you set to input field it's equal entered value by you display. (like; http://127.0.0.1:8000/?username=hello) 
 
-# so one important thing is whenever you submit any type of form then it was always be a pass with "GET" request and You can see this on browser HEader Section.
+# so one important thing is whenever you submit any type of form then it was always be a pass with "GET" request and 
+# You can see this on browser Header Section to see it goto networks and header section in your browser.
 # Another method is 'POST'(Collect user data) method which use when we not want to send some data but want get data from server.
-# Then we change form method to post then you see in url can't see what use submited
+# Then we change form method to post then you see in url can't see what user submited
 # also getting one error 'forbidden csrf token' 
 
 # CSRF: CROSS SIDE REQUEST FORGEWRIEY
@@ -23,6 +27,17 @@
 # Also token was every time diffrent so no need to wory to see goto inspect and there it is.
 
 # for generate csrf token just goto your form tag and add: {% csrf_token %}
+# So the conclution is whenever yu submitt the data in form using post method need to add csrf tocken.
+
+
 
 # Handling form submission and extracting data:
+# So here at the time of form submission to transfer from http://127.0.0.1:8000/ to http://127.0.0.1:8000/ we add action attribute in form tag.
+# Here we add just '/' if we added '/add-on' then it will redirect to http://127.0.0.1:8000/add-on/
+# So we want to capture data so first we identify by which request(GET/POST) using the form was submitted.
+# Then take require action. Here we simply on submiting form render thank you page.
+ 
 
+# Manually form validation and problem with "that":
+# If there is nothing written inside the form and user submit form then we want to show some error news 
+# Handling form validation is typically very lenthy task i added one example here. 
