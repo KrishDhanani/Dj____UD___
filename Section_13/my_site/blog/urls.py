803f237e0dd0b42cdcdd619584e5e2bc42e4e59f
Krish <krishdhanani9989@gmail.com>
2024-09-02 19:52:33 +0530

    2 Sep

--- Section_13/my_site/blog/urls.py
+++ Section_13/my_site/blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts/", views.AllPostView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")  # posts/my-first-post
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),  # posts/my-first-post
     # instead of use slug you can also use unique id but that was less SEO(Search engine optimization) friendly so use like dash keyword
     # slug transformer(transformer like int, str, slug) basically check the character and in special character only dash(-) allow and if all character match or not
+    path('read-later', views.ReadLaterView.as_view(), name='read-later')
 ]
--- Section_13/my_site/blog/views.py
+++ Section_13/my_site/blog/views.py
@@ -34,7 +34,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -54,7 +55,40 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, 'blog/post-detail.html', context)
-    fbjheawbrfhjeb
+    
+# Making Read later via Session
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get('stored_post')
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+
+    def post(self,request):
+        stored_posts = request.session.get('stored_post')
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST['post_id'])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+
+        return HttpResponseRedirect("/")
+ 
